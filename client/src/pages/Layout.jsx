c61d712e2a2650a69afb3262ab40ab542cd7b0e5
olana16 <oligeda16@gmail.com>
2026-02-09 11:07:44 +0300

    sign in with clerck created

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -6,17 +6,26 @@ import { Outlet } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { loadTheme } from '../features/themeSlice'
 import { Loader2Icon } from 'lucide-react'
-import {userUser, SignIn} from '@clerk/clerk-react'
+import {SignIn, useUser} from '@clerk/clerk-react'
 
 const Layout = () => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(false)
     const { loading } = useSelector((state) => state.workspace)
     const dispatch = useDispatch()
+    const {user, isLoaded} = useUser()
 
     // Initial load of theme
     useEffect(() => {
         dispatch(loadTheme())
     }, [])
+
+     if(!user){
+        return(
+            <div className='flex justify-center items-center h-screen dark:bg-zinc-950'>
+              <SignIn />
+            </div>
+        )
+     }
 
     if (loading) return (
         <div className='flex items-center justify-center h-screen bg-white dark:bg-zinc-950'>
