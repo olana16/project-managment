f50068ea01e85c41fba248b3cbfb3525dc1847bc
olana16 <oligeda16@gmail.com>
2026-02-10 11:17:35 +0300

    update

--- server/configs/prisma.js
+++ server/configs/prisma.js
@@ -1,24 +1,30 @@
 import 'dotenv/config'
 import { PrismaClient } from '@prisma/client'
 import { PrismaNeon } from '@prisma/adapter-neon'
-import { neonConfig } from '@neondatabase/serveerless'
-
+import { Pool, neonConfig } from '@neondatabase/serverless'
 import ws from 'ws'
+
+// Required for Neon WebSocket connections in Node.js
 neonConfig.webSocketConstructor = ws
 
+// Create a Neon pool
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+})
 
+// Create PrismaNeon adapter
+const adapter = new PrismaNeon(pool)
 
+// Global caching for dev hot-reload
+const globalForPrisma = globalThis
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+  })
 
-
-
-
-
-
-const connectionString =  `${process.env.DATABASE_URL}`
-
-const adapter =  new PrismaNeon({connectionString})  
-const prisma = global.prisma || new PrismaClient({adapter})
-
-if(process.env.NODE_ENV === 'development') global.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma
+}
 
 export default prisma
