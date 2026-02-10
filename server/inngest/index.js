3f64ff4e240ee37dd82942877246ff404539d703
olana16 <oligeda16@gmail.com>
2026-02-10 14:30:52 +0300

    inngest function to create user created

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,7 +1,26 @@
 import { Inngest } from "inngest";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "my-app" });
+export const inngest = new Inngest({ id: "project-managment" });
+
+const syncUserCreation = inngest.createFunction(
+    {id: 'sync-user-from-clerk'},
+    {event: 'clerk/user.created'},
+    async({event})=>{
+        const {data} = event
+        await prisma.user.create({
+            data:{
+                id: data.id,
+                email: data.email_addresses[0]?.email_addres,
+                name: data?.first_name + " " + data?.last_name,
+                image: data?.image_url,
+                
+
+            }
+        })
+    }
+)
+
 
 // Create an empty array where we'll export future Inngest functions
 export const functions = [];
