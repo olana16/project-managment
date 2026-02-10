63c5b0d949d6ae53d6c525505ca3266d303e46a6
olana16 <oligeda16@gmail.com>
2026-02-10 14:35:35 +0300

    inngest function to delete user from database

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -3,6 +3,8 @@ import { Inngest } from "inngest";
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "project-managment" });
 
+
+//inngest function to save user data to a database
 const syncUserCreation = inngest.createFunction(
     {id: 'sync-user-from-clerk'},
     {event: 'clerk/user.created'},
@@ -14,12 +16,27 @@ const syncUserCreation = inngest.createFunction(
                 email: data.email_addresses[0]?.email_addres,
                 name: data?.first_name + " " + data?.last_name,
                 image: data?.image_url,
-                
+
 
             }
         })
     }
 )
+
+//inngest function to delete user from database
+const syncUserDeletion = inngest.createFunction(
+    {id: 'delete-user-from-clerk'},
+    {event: 'clerk/user.deleted'},
+    async({event})=>{
+        const {data} = event
+        await prisma.user.create({
+            where:{
+                id: data.id,
+            }
+        })
+    }
+)
+
 
 
 // Create an empty array where we'll export future Inngest functions
